665cd8a65f8c1d78e0674edbff356f16eb5c3daf
sebastian rueda <sebastianruedaolarte@gmail.com>
2022-11-07 20:58:44 -0300

    fix commit

---
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { ApiService } from "../../services/api.service";
+import { Router } from "@angular/router";
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html'
+})
+export class SignInComponent {
+  constructor(private apiService: ApiService, private router: Router) { }
+
+  signIn() {
+    this.router.navigate(['/home']);
+  }
+
+  signUp() {
+    this.router.navigate(['/sign-up']);
+  }
+}
