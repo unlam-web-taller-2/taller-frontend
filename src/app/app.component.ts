26cf3f50dbb2522ea4e68fc83f5564915ea0f33c
sebastian rueda <sebastianruedaolarte@gmail.com>
2022-11-18 22:19:34 -0300

    add favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-  title = 'miAplicacion';
+export class AppComponent {
 
   constructor() {}
-
-  ngOnInit(): void {
-
-  }
 }
